d8600bac4db9124b22188bfd7ab1357da4185f1c
Julien Puyobro <julien.puyobro@ubisoft.com>
2024-11-05 14:10:16 +0000

    feat: Add initial code for flexmetal provider

---
+++ internal/provider/provider.go
@@ -0,0 +1,43 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+var _ provider.Provider = (*flexmetalProvider)(nil)
+
+func New() func() provider.Provider {
+	return func() provider.Provider {
+		return &flexmetalProvider{}
+	}
+}
+
+type flexmetalProvider struct{}
+
+func (p *flexmetalProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+
+}
+
+func (p *flexmetalProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+
+}
+
+func (p *flexmetalProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "flexmetal"
+}
+
+func (p *flexmetalProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{
+		NewServersDataSource,
+	}
+}
+
+func (p *flexmetalProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{
+		NewServerResource,
+	}
+}
