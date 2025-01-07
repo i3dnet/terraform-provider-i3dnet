06ad5e8a687ec8d0cb21f8f1512467a676270b5d
Andrei Boar <aboar@ext.scaleway.com>
2025-01-07 20:26:40 +0200

    prepare acceptance test for data source ssh key

---
+++ internal/provider/ssh_key_data_source_test.go
@@ -0,0 +1,34 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccSSHKeyDataSource(t *testing.T) {
+	// todo: This test requires a key with name `Demo` to be available on the test system. Once we have env variables
+	//  passed into tests create this key via API client before running the test
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Read testing
+			{
+				Config: providerConfig + `
+data "i3d_ssh_key" "example" {
+  name = "Demo"
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify attributes
+					resource.TestCheckResourceAttr("data.i3d_ssh_key.example", "name", "Demo"),
+					// Verify dynamic values have any value set in the state.
+					resource.TestCheckResourceAttrSet("data.i3d_ssh_key.example", "uuid"),
+					resource.TestCheckResourceAttrSet("data.i3d_ssh_key.example", "public_key"),
+					resource.TestCheckResourceAttrSet("data.i3d_ssh_key.example", "created_at"),
+				),
+			},
+		},
+	})
+}
