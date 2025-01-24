627b3487f8184aa28000ada3947568cf5d0df054
Andrei Boar <andrey.boar@gmail.com>
2025-01-24 13:52:37 +0200

    update docs

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -15,7 +15,20 @@ provider "i3dnet" {
   api_key = var.i3dnet_api_key
 }
 
-# Create a flexmetal server
+# Create your SSH key
+resource "i3dnet_ssh_key" "ssh" {
+  name       = "MyPublicKey"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIER64HsjCSspx/JMhHELr8LgYwW/PdFrfj7Kr6UM76WS john.doe@email.com"
+}
+
+
+# Create an Ubuntu Server
 resource "i3dnet_flexmetal_server" "my-server" {
-  # ...
+  name          = "TerraFlex-Server"
+  location      = "EU: Rotterdam"
+  instance_type = "bm7.std.8"
+  os = {
+    slug = "ubuntu-2404-lts"
+  }
+  ssh_key = [i3dnet_ssh_key.ssh.public_key]
 }
